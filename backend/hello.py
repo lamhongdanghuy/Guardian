15c5366fc691ff92206647dac66024a820e52fd7
Albert Luna <luna.alvr1@gmail.com>
2024-01-24 21:13:31 -0600

    frontend backend interactions - basics

--- backend/hello.py
+++ backend/hello.py
@@ -1,6 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+import json
 
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
 def hello():
-    print ("hello world")
+    print ("Got Request")
+    return json.dumps("Hello World - From the Backend")
 
-hello()
+if __name__ == "__main__":
+    app.run()
+
+#Runs on localhost:5000
--- guardianFE/src/LandingPage.tsx
+++ guardianFE/src/LandingPage.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import Header from './Header'
+import { useState } from "react";
+import Header from "./Header";
 
 function LandingPage() {
+  const [message, setMessage] = useState("");
+
+  const fetchHello = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/"); // Update the URL with your Flask API endpoint
+      const result = await response.json();
+      console.log("got data");
+      setMessage(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <>
-    <Header/>
+      <Header />
+      <button onClick={fetchHello}>Talk to backend!</button>
+      {message}
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
