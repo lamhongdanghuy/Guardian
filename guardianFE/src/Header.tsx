13ea7f6e75ddc78233c0cbd959aa1667e8d5ad18
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-01-25 23:40:06 -0600

    part 1 landing page
    No pictures or services section yet.

--- guardianFE/src/Header.tsx
+++ guardianFE/src/Header.tsx
@@ -1,19 +1,24 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 function Header() {
-
   return (
     <>
-    <header>
-      <h1>DePaul Guardian</h1>
-      <div className='NavBar'>
-      <Link to="/">Home</Link>
-      <Link to= "/apply">Apply</Link>
-      <Link to="/about">About</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      </div>
-    </header>
+      <header>
+        <Link
+          to="/login"
+          style={{ marginLeft: "auto", marginRight: "2em", marginBottom: "0" }}
+        >
+          Log In
+        </Link>
+        <h1>DePaul Guardian</h1>
+        <div className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/apply">Apply</Link>
+          <Link to="/about">About</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
