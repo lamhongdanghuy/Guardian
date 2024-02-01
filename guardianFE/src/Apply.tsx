6d1b19b919671164283008591f95d065912e4ed2
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-02-01 14:09:11 -0600

    fixes

--- guardianFE/src/Apply.tsx
+++ guardianFE/src/Apply.tsx
@@ -4,28 +4,30 @@ import Header from "./Header";
 
 const Apply: React.FC = () => {
   return (
-    <div className="clientApply">
+    <>
       <Header />
-      <h1>Apply</h1>
-
-      <div className="studentApply">
-        <h2>Students</h2>
-        <p>
-          If you are a student at DePaul University and are interested in
-          joining the DePaul Guardian, please fill out the form below.
-        </p>
-        <Link to="/apply/student">Apply as Student</Link>
-      </div>
-
       <div className="clientApply">
-        <h2>Clients</h2>
-        <p>
-          If you are a client and are interested in working with the DePaul
-          Guardian, please fill out the form below.
-        </p>
-        <Link to="/apply/client">Apply as Client</Link>
+        <h1>Apply</h1>
+
+        <div className="studentApply">
+          <h2>Students</h2>
+          <p>
+            If you are a student at DePaul University and are interested in
+            joining the DePaul Guardian, please fill out the form below.
+          </p>
+          <Link to="/apply/student">Apply as Student</Link>
+        </div>
+
+        <div className="clientApply">
+          <h2>Clients</h2>
+          <p>
+            If you are a client and are interested in working with the DePaul
+            Guardian, please fill out the form below.
+          </p>
+          <Link to="/apply/client">Apply as Client</Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
