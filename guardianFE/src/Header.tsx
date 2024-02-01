7ef91cff692394bc773cfdad51d90453fdaf1d6e
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-01-31 23:36:15 -0600

    services part 1

--- guardianFE/src/Header.tsx
+++ guardianFE/src/Header.tsx
@@ -1,21 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 function Header() {
+  const [serviceOpen, setServiceOpen] = useState(false);
   return (
     <>
       <header>
         <Link
           to="/login"
-          style={{ marginLeft: "auto", marginRight: "2em", marginBottom: "0" }}
+          style={{
+            marginLeft: "auto",
+            marginRight: "2em",
+            marginBottom: "0",
+          }}
         >
           Log In
         </Link>
         <h1>DePaul Guardian</h1>
-        <div className="navbar">
+        <nav>
           <Link to="/">Home</Link>
           <Link to="/apply">Apply</Link>
           <Link to="/about">About</Link>
-          <Link to="/dashboard">Dashboard</Link>
-        </div>
+          <div
+            onMouseEnter={() => setServiceOpen(true)}
+            onMouseLeave={() => setServiceOpen(false)}
+          >
+            <NavLink to="/services">Services</NavLink>
+            {serviceOpen && (
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <Link to="/services/service1">Service 1</Link>
+                <Link to="/services/service2">Service 2</Link>
+                <Link to="/services/service3">Service 3</Link>
+              </div>
+            )}
+          </div>
+        </nav>
       </header>
     </>
   );
