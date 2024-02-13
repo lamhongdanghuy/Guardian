014200cf5815c0583d5da1ef076d5d8f8f373a9e
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-02-12 19:40:02 -0600

    project cards and project expanded view

---
+++ guardianFE/src/ProjectInfoView.tsx
@@ -0,0 +1,14 @@
+function ProjectInfoView() {
+  return (
+    <div>
+      <h1>Project Name</h1>
+      <h1>TYPE</h1>
+      <h1>Description</h1>
+      <h1>Status</h1>
+      <h1>Target Date</h1>
+      <h1>Project Leader</h1>
+    </div>
+  );
+}
+
+export default ProjectInfoView;
--- guardianFE/src/ProjectsView.tsx
+++ guardianFE/src/ProjectsView.tsx
@@ -1,7 +1,74 @@
-function ProjectsView() {
+import ProjectCard from "./ProjectCard";
+
+interface projectViewProp {
+  onClick: Function;
+}
+
+function ProjectsView(props: projectViewProp) {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div
+      style={{
+        marginTop: "12.5vh",
+        maxHeight: "87.5vh",
+        overflow: "hidden",
+      }}
+    >
+      <h1 style={{ fontSize: "10vh" }}>Projects</h1>
+      <div
+        style={{
+          margin: "0 5vw",
+          display: "flex",
+          flexWrap: "wrap",
+          textAlign: "center",
+          overflowY: "scroll",
+          maxHeight: "70vh",
+          marginBottom: "5vh",
+          gap: "5vh",
+        }}
+      >
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+        <ProjectCard
+          name={"Project Name"}
+          status={"Current Status"}
+          projectLeader={"PERSON"}
+          type={"GRA"}
+          onClick={props.onClick}
+        ></ProjectCard>
+      </div>
     </div>
   );
 }
