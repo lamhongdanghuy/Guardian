2f3e1d6ff5a82f5595238e22b69606d71f918f4a
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-02-22 14:52:37 -0600

    update addFacultyRoute

--- guardianFE/src/AddFaculty.tsx
+++ guardianFE/src/AddFaculty.tsx
@@ -30,7 +30,7 @@ function AddFaculty() {
       );
       return;
     }
-    const response = await fetch("http://localhost:5000/propose", {
+    const response = await fetch("http://localhost:5000/addFaculty", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
