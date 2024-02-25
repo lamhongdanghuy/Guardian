bd3432e339e99d77c3cb6635c8315e92bfe0188c
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-02-25 00:14:09 -0600

    project card styling

--- guardianFE/src/ProjectCard.tsx
+++ guardianFE/src/ProjectCard.tsx
@@ -16,19 +16,29 @@ function ProjectCard(props: ProjectCardProps) {
     <div className="card" onClick={handleClick}>
       <div className="cardTopProject">
         <h2
-          style={{ fontSize: "24px", marginLeft: "1vw", marginRight: "auto" }}
+          style={{
+            fontSize: "20px",
+            marginLeft: "1vw",
+            marginRight: "auto",
+            width: "60%",
+          }}
         >
           {props.name}
         </h2>
         <h2
-          style={{ fontSize: "24px", marginRight: "1vw", marginLeft: "auto" }}
+          style={{
+            fontSize: "20px",
+            marginRight: "1vw",
+            marginLeft: "auto",
+            width: "40%",
+          }}
         >
           {props.type}
         </h2>
       </div>
       <div className="cardBottom">
-        <h2 style={{ fontSize: "24px" }}>{props.status}</h2>
-        <h2 style={{ fontSize: "24px" }}>Leader: {props.projectLeader}</h2>
+        <h2 style={{ fontSize: "20px" }}>{props.status}</h2>
+        <h2 style={{ fontSize: "20px" }}>Leader: {props.projectLeader}</h2>
       </div>
     </div>
   );
