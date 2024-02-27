ff3f5ac2134aad2457ace5bfcb26e26c45e6919b
Huy Lam <lamhongdanghuy@gmail.com>
2024-02-26 22:59:37 -0600

    can not add the same student twice on the front end

--- guardianFE/src/ProjectInfoView.tsx
+++ guardianFE/src/ProjectInfoView.tsx
@@ -23,15 +23,16 @@ function ProjectInfoView(projectID: string) {
 
   const addStudent = async (stu: Member) => {
     const response = await fetch("http://localhost:5000/addStudent", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        token: user.token ? user.token : "",
-      },
-      body: JSON.stringify({ projectID, student: stu }),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            token: user.token ? user.token : "",
+        },
+        body: JSON.stringify({ projectID, student: stu }),
     });
     setAssignedStudents([...assigned_students, stu]);
-  };
+    await getProjectInfo();
+};
 
   const getProjectInfo = async () => {
     const response = await fetch("http://localhost:5000/projectInfo", {
