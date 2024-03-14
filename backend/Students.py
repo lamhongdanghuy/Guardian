11d02e2460a863b486c9d7480284dcfa759bb388
LargeUmbrella <amenakploh@gmail.com>
2024-03-14 00:54:24 -0500

    more comments

--- backend/Students.py
+++ backend/Students.py
@@ -1,11 +1,11 @@
 class Students:
 
     def get_students(self, db_Connection):
+        # Gets the entire list of registered students
         query = """
             SELECT *
             FROM STUDENT;
             """
-        
         print(query)
         projectData = db_Connection.select_query(query)
         if projectData.empty:
