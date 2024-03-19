049be9573b6fa31499d062ce0e06ad643618f474
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2024-03-19 14:26:18 -0500

    new dist deploy 2:26pm

--- guardianFE/src/fetchApiURL.ts
+++ guardianFE/src/fetchApiURL.ts
@@ -1,6 +1,6 @@
 // API URL shortcut. Allows quick swiching between local and deployed API URLs.
 // Contributors: Joel Chamakala
 
-const API_BASE_URL: String = "http://localhost:5000";
+const API_BASE_URL: String = "http://18.216.233.27:5000";
 
 export default API_BASE_URL;
